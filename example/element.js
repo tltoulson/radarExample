93e15032b9e0549ffc7bcd42040b7ed404810fbe
tltoulson <tltoulson@gmail.com>
2024-05-14 10:33:34 -0400

    Fixed close tag

--- example/element.js
+++ example/element.js
@@ -5,5 +5,5 @@ const el = document.createElement('DIV');
 document.body.appendChild(el);
 
 el.innerHTML = `		
-<x-0505-radar-example></x-0505-radar-test>
+<x-0505-radar-example></x-0505-radar-example>
 `;
